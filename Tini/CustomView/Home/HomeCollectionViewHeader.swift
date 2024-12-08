3794ed77ca4bfcff80153603b72c1c97a547fdf3
omodauda <omodauda.dl@gmail.com>
2024-12-08 18:52:06 +0100

    Remove ScrollView for CollectionView

---
+++ Tini/CustomView/Home/HomeCollectionViewHeader.swift
@@ -0,0 +1,12 @@
+//
+//  HomeCollectionViewHeader.swift
+//  Tini
+//
+//  Created by Omodauda on 08/12/2024.
+//
+
+import UIKit
+
+class HomeCollectionViewHeader: UICollectionReusableView {
+        
+}
