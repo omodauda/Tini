bb7f351622889cf70ed4eb11abd30aee26aec0a3
omodauda <omodauda.dl@gmail.com>
2024-12-22 17:44:13 +0100

    store model

---
+++ Tini/Models/StoreModel.swift
@@ -0,0 +1,19 @@
+//
+//  StoreModel.swift
+//  Tini
+//
+//  Created by Omodauda on 22/12/2024.
+//
+
+import Foundation
+
+struct StoreModel {
+    let name: String
+    let address: String
+    let isFavorite: Bool
+}
+
+struct StoresGroup {
+    let title: String
+    var stores: [StoreModel]
+}
