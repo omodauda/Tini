7396e4762a6054f3815aa0e6861a3ca4c74ad2c6
omodauda <omodauda.dl@gmail.com>
2024-12-24 17:45:11 +0100

    Search Product VC

---
+++ Tini/Controllers/SearchProductVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchProductVC.swift
+//  Tini
+//
+//  Created by Omodauda on 24/12/2024.
+//
+
+import UIKit
+
+class SearchProductVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
