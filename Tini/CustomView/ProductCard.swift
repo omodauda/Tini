4b69d2e05b803be1429ecc4b114f962e4202fa71
omodauda <omodauda.dl@gmail.com>
2024-12-28 13:29:27 +0100

    product card

---
+++ Tini/CustomView/ProductCard.swift
@@ -0,0 +1,20 @@
+//
+//  ProductCard.swift
+//  Tini
+//
+//  Created by Omodauda on 28/12/2024.
+//
+
+import UIKit
+
+class ProductCard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
