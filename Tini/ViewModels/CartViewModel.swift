9147dd6fd82778ba9e1c4a2090f06a1892705f8a
omodauda <omodauda.dl@gmail.com>
2025-01-01 13:47:26 +0100

    update cart view model

--- Tini/ViewModels/CartViewModel.swift
+++ Tini/ViewModels/CartViewModel.swift
@@ -10,7 +10,33 @@ import Foundation
 class CartViewModel {
     
     var cartItems: [CartItemModel] = [
-        .init(productImage: Images.Products.capuccino!, productName: "Capuccino", size: "Small", amount: 69.00, quantity: 1, addOns: nil),
-        .init(productImage: Images.Products.smokyBurger!, productName: "Smoky burger", size: "Small", amount: 250.00, quantity: 1, addOns: ["Double-patty", "Emmento"]),
+        .init(productImage: Images.Products.capuccino!, productName: "Capuccino", size: "Small", amount: 69.00, quantity: 1, addOns: nil, totalPrice: 69.00),
+        .init(productImage: Images.Products.smokyBurger!, productName: "Smoky burger", size: "Small", amount: 250.00, quantity: 1, addOns: ["Double-patty", "Emmento"], totalPrice: 250.00),
     ]
+    
+    var cartTotal: Double {
+        var total: Double = 0.0
+        
+        for item in cartItems {
+            total += item.totalPrice
+        }
+        return total
+    }
+    
+    var shippingFee: Double {
+        return (5 / 100) * cartTotal
+    }
+    
+    func increaseQty(for index: Int) {
+        cartItems[index].quantity += 1
+        let itemPrice = cartItems[index].amount
+        cartItems[index].totalPrice += itemPrice
+    }
+    
+    func decreaseQty(for index: Int) {
+        guard cartItems[index].quantity > 1 else { return }
+        cartItems[index].quantity -= 1
+        let itemPrice = cartItems[index].amount
+        cartItems[index].totalPrice -= itemPrice
+    }
 }
