da26e3bb2cb4aa9d21302338e2221b9a7c225c8a
omodauda <omodauda.dl@gmail.com>
2025-01-01 15:29:04 +0100

    cart pickup view

---
+++ Tini/CustomView/CartPickupView.swift
@@ -0,0 +1,20 @@
+//
+//  CartPickupView.swift
+//  Tini
+//
+//  Created by Omodauda on 01/01/2025.
+//
+
+import UIKit
+
+class CartPickupView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
