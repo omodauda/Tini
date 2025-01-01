67fdf18be5342ade671a969c7738c07708d97eb3
omodauda <omodauda.dl@gmail.com>
2025-01-01 12:05:17 +0100

    cart item model

---
+++ Tini/Models/CartItemModel.swift
@@ -0,0 +1,18 @@
+//
+//  CartItem.swift
+//  Tini
+//
+//  Created by Omodauda on 31/12/2024.
+//
+
+import UIKit
+
+struct CartItemModel {
+    let productImage: UIImage
+    let productName: String
+    let size: String
+    let amount: Double
+    let quantity: Int
+    let addOns: [String]?
+}
+
