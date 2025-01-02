ed266cd282a3cd024dfac45e6887baf7059a2240
omodauda <omodauda.dl@gmail.com>
2025-01-02 19:12:41 +0100

    update cart item model

--- Tini/Models/CartItemModel.swift
+++ Tini/Models/CartItemModel.swift
@@ -10,7 +10,7 @@ import UIKit
 struct CartItemModel {
     let productImage: UIImage
     let productName: String
-    let size: String
+    let size: String?
     let amount: Double
     var quantity: Int
     let addOns: [String]?
