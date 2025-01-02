cde58ec297ee9ab451eed28797135b14dffd1fe9
omodauda <omodauda.dl@gmail.com>
2025-01-02 12:33:55 +0100

    modal view header custom view

---
+++ Tini/CustomView/ModalHeaderView.swift
@@ -0,0 +1,85 @@
+//
+//  ModalHeaderView.swift
+//  Tini
+//
+//  Created by Omodauda on 01/01/2025.
+//
+
+import UIKit
+
+protocol ModalHeaderViewDelegate: AnyObject {
+    func didTapDismissButton()
+}
+
+class ModalHeaderView: UIView {
+
+    private let title: String
+    
+    weak var delegate: ModalHeaderViewDelegate?
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Pickup Time"
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.textColor = UIColor(hex: Colors.titleText)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let dismissButton: UIButton = {
+        let button = UIButton()
+        button.setImage(Images.closeIcon, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = UIColor(hex: Colors.titleText)
+        return button
+    }()
+    
+    private let separator: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hex: Colors.tetiary)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    init(title: String) {
+        self.title = title
+        super.init(frame: .zero)
+        
+        setupUI()
+        configureDismissButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureDismissButton() {
+        dismissButton.addTarget(self, action: #selector(dismissTapped), for: .touchUpInside)
+    }
+    
+    @objc private func dismissTapped() {
+        delegate?.didTapDismissButton()
+    }
+    
+    private func setupUI() {
+        addSubview(titleLabel)
+        addSubview(dismissButton)
+        addSubview(separator)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            dismissButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+            dismissButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -21),
+            dismissButton.widthAnchor.constraint(equalToConstant: 14),
+            dismissButton.heightAnchor.constraint(equalToConstant: 14),
+            
+            separator.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            separator.leadingAnchor.constraint(equalTo: leadingAnchor),
+            separator.trailingAnchor.constraint(equalTo: trailingAnchor),
+            separator.bottomAnchor.constraint(equalTo: bottomAnchor),
+            separator.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+}
