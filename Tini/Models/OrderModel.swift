c8cb6848e353a52da9df69da7e7d2bc38acc1a34
omodauda <omodauda.dl@gmail.com>
2025-04-17 14:16:45 +0100

    update order model

--- Tini/Models/OrderModel.swift
+++ Tini/Models/OrderModel.swift
@@ -5,4 +5,32 @@
 //  Created by Omodauda on 16/04/2025.
 //
 
-import Foundation
+import UIKit
+
+enum OrderStatus: String {
+    case received
+    case preparing
+    case readyForPickup
+    case delivering
+    case delivered
+    case failed
+    case completed
+}
+
+enum OrderType: String {
+    case pickup
+    case delivery
+}
+
+struct OrderModel {
+    let status: OrderStatus
+    let createdAt: String
+    let type: OrderType
+    let storeAddress: String
+    let deliveryDate: String?
+    let deliveryAddress: String?
+    let total: Double
+    let shippingFee: Double
+    let promotion: Double
+    let items: [CartItemModel]
+}
