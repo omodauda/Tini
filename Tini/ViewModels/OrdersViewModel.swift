8cf99f4b286da9a756a6adba9b9354310b4b1c50
omodauda <omodauda.dl@gmail.com>
2025-04-17 14:15:42 +0100

    oders view model

---
+++ Tini/ViewModels/OrdersViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  OrdersViewModel.swift
+//  Tini
+//
+//  Created by Omodauda on 16/04/2025.
+//
+
+import Foundation
+
+class OrdersViewModel {
+    static let shared = OrdersViewModel()
+    
+    private init() {}
+    
+    var orders: [OrderModel] = [
+        OrderModel(status: .preparing, createdAt: "20/04/2025, 04:20", type: .delivery, storeAddress: "13 Han Thuyen, D.1, HCM city", deliveryDate: nil, deliveryAddress: "285 CMT8, D.10, HCM city", total: 284.00, shippingFee: 15.00, promotion: 50.00, items: [
+            CartItemModel(productImage: Images.Products.capuccino!, productName: "Capuccino", size: "Small", amount: 69.00, quantity: 1, addOns: nil, totalPrice: 69.00),
+            CartItemModel(productImage: Images.Products.smokyBurger!, productName: "Smoky burger", size: nil, amount: 250.00, quantity: 1, addOns: ["Double-patty", "Emmento"], totalPrice: 250.00),
+        ]),
+        OrderModel(status: .delivering, createdAt: "20/04/2025, 04:20", type: .delivery, storeAddress: "13 Han Thuyen, D.1, HCM city", deliveryDate: nil, deliveryAddress: "285 CMT8, D.10, HCM city", total: 284.00, shippingFee: 15.00, promotion: 50.00, items: [
+            CartItemModel(productImage: Images.Products.capuccino!, productName: "Capuccino", size: "Small", amount: 69.00, quantity: 1, addOns: nil, totalPrice: 69.00),
+            CartItemModel(productImage: Images.Products.smokyBurger!, productName: "Smoky burger", size: nil, amount: 250.00, quantity: 1, addOns: ["Double-patty", "Emmento"], totalPrice: 250.00),
+        ]),
+        OrderModel(status: .delivered, createdAt: "20/04/2025, 04:20", type: .delivery, storeAddress: "13 Han Thuyen, D.1, HCM city", deliveryDate: nil, deliveryAddress: "285 CMT8, D.10, HCM city", total: 284.00, shippingFee: 15.00, promotion: 50.00, items: [
+            CartItemModel(productImage: Images.Products.capuccino!, productName: "Capuccino", size: "Small", amount: 69.00, quantity: 1, addOns: nil, totalPrice: 69.00),
+            CartItemModel(productImage: Images.Products.smokyBurger!, productName: "Smoky burger", size: nil, amount: 250.00, quantity: 1, addOns: ["Double-patty", "Emmento"], totalPrice: 250.00),
+        ]),
+        OrderModel(status: .failed, createdAt: "20/04/2025, 04:20", type: .delivery, storeAddress: "13 Han Thuyen, D.1, HCM city", deliveryDate: nil, deliveryAddress: "285 CMT8, D.10, HCM city", total: 284.00, shippingFee: 15.00, promotion: 50.00, items: [
+            CartItemModel(productImage: Images.Products.capuccino!, productName: "Capuccino", size: "Small", amount: 69.00, quantity: 1, addOns: nil, totalPrice: 69.00),
+            CartItemModel(productImage: Images.Products.smokyBurger!, productName: "Smoky burger", size: nil, amount: 250.00, quantity: 1, addOns: ["Double-patty", "Emmento"], totalPrice: 250.00),
+        ]),
+        
+        OrderModel(status: .received, createdAt: "20/04/2025, 04:20", type: .pickup, storeAddress: "13 Han Thuyen, D.1, HCM city", deliveryDate: "10:00, Today", deliveryAddress: nil, total: 284.00, shippingFee: 15.00, promotion: 50.00, items: [
+            CartItemModel(productImage: Images.Products.capuccino!, productName: "Capuccino", size: "Small", amount: 69.00, quantity: 1, addOns: nil, totalPrice: 69.00),
+            CartItemModel(productImage: Images.Products.smokyBurger!, productName: "Smoky burger", size: nil, amount: 250.00, quantity: 1, addOns: ["Double-patty", "Emmento"], totalPrice: 250.00),
+        ]),
+        OrderModel(status: .readyForPickup, createdAt: "20/04/2025, 04:20", type: .pickup, storeAddress: "13 Han Thuyen, D.1, HCM city", deliveryDate: "10:00, Today", deliveryAddress: nil, total: 284.00, shippingFee: 15.00, promotion: 50.00, items: [
+            CartItemModel(productImage: Images.Products.capuccino!, productName: "Capuccino", size: "Small", amount: 69.00, quantity: 1, addOns: nil, totalPrice: 69.00),
+            CartItemModel(productImage: Images.Products.smokyBurger!, productName: "Smoky burger", size: nil, amount: 250.00, quantity: 1, addOns: ["Double-patty", "Emmento"], totalPrice: 250.00),
+        ]),
+        OrderModel(status: .completed, createdAt: "20/04/2025, 04:20", type: .pickup, storeAddress: "13 Han Thuyen, D.1, HCM city", deliveryDate: "10:00, Today", deliveryAddress: nil, total: 284.00, shippingFee: 15.00, promotion: 50.00, items: [
+            CartItemModel(productImage: Images.Products.capuccino!, productName: "Capuccino", size: "Small", amount: 69.00, quantity: 1, addOns: nil, totalPrice: 69.00),
+            CartItemModel(productImage: Images.Products.smokyBurger!, productName: "Smoky burger", size: nil, amount: 250.00, quantity: 1, addOns: ["Double-patty", "Emmento"], totalPrice: 250.00),
+        ]),
+    ]
+}
