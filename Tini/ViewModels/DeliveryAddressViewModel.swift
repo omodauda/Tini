c4e7a0742fc20528cbb00a9b3f6d66fc10b347d7
omodauda <omodauda.dl@gmail.com>
2025-05-06 15:49:15 +0100

    Create delivery address view model publisher

--- Tini/ViewModels/DeliveryAddressViewModel.swift
+++ Tini/ViewModels/DeliveryAddressViewModel.swift
@@ -6,13 +6,28 @@
 //
 
 import Foundation
+import Combine
 
 struct DeliveryAddressViewModel {
     
     static let shared = DeliveryAddressViewModel()
     
-    let deliveryAddress: [DeliveryAddressModel] = [
-        .init(address: "128 Nguyen Dinh Chieu, D.3, HCM", recipientName: "Nick", recipientPhoneNumber: "0896969696"),
-        .init(address: "128 Nguyen Dinh Chieu, D.3, HCM", recipientName: "Tu", recipientPhoneNumber: "0896969696"),
-    ]
+    private var deliveryAddressSubject = CurrentValueSubject<[DeliveryAddressModel], Never>([
+        .init(address: "128 Nguyen Dinh Chieu", city: "Ho Chi Minh", district: "District 1", ward: "Ward A", recipientName: "Nick", recipientPhoneNumber: "0896969696"),
+        .init(address: "128 Nguyen Dinh Chieu", city: "Hanoi", district: "Ba Dinh", ward: "Ward 1", recipientName: "Tu", recipientPhoneNumber: "0896969696"),
+    ])
+    
+    var deliveryAddressPublisher: AnyPublisher<[DeliveryAddressModel], Never>{
+        deliveryAddressSubject.eraseToAnyPublisher()
+    }
+    
+//    let deliveryAddress: [DeliveryAddressModel] = [
+//        .init(address: "128 Nguyen Dinh Chieu, D.3, HCM", recipientName: "Nick", recipientPhoneNumber: "0896969696"),
+//        .init(address: "128 Nguyen Dinh Chieu, D.3, HCM", recipientName: "Tu", recipientPhoneNumber: "0896969696"),
+//    ]
+    func addAddress(address: DeliveryAddressModel) {
+        var currentAddresses = deliveryAddressSubject.value
+        currentAddresses.append(address)
+        deliveryAddressSubject.send(currentAddresses)
+    }
 }
