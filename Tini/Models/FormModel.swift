f799770ddd1e4780f05c634d4972f01b7fa21498
omodauda <omodauda.dl@gmail.com>
2025-05-13 14:28:44 +0100

    Form Model

---
+++ Tini/Models/FormModel.swift
@@ -0,0 +1,15 @@
+//
+//  FormModel.swift
+//  Tini
+//
+//  Created by Omodauda on 13/05/2025.
+//
+
+import Foundation
+import UIKit
+
+struct FormField {
+    let label: UILabel
+    let input: UITextField
+    let errorLabel: UILabel
+}
